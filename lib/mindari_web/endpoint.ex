3b987cda43929c2c283bb093cda4fcf213f92705
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2026-03-24 20:43:26 -0400

    Remove CORS

--- lib/mindari_web/endpoint.ex
+++ lib/mindari_web/endpoint.ex
@@ -47,6 +47,11 @@ defmodule MindariWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug :add_cors_headers
+  defp add_cors_headers(conn, _opts) do
+    put_resp_header(conn, "Access-Control-Allow-Origin", "*")
+  end
+
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
